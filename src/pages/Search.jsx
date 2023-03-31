3e15c2eee441ce87ee15dab7da6ccf5bac04c991
Vero Conesa <vero.gconesa@gmail.com>
2023-03-31 16:13:18 +0200

    🆕 feat Create Search Component

---
+++ src/pages/Search.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+
+export default function SearchPage ({ routeParams }) {
+  useEffect(() => {
+    document.title = `Searching ${routeParams.query}`
+  }, [])
+  return (
+    <h1>Searching {routeParams.query}</h1>
+  )
+}
